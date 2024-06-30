05e6709e3cfed2787f0856abe807c4c2cbdc9319
baishakhee93 <baishu9534@gmail.com>
2024-06-30 13:33:58 +0530

    color change1

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PDF Generator',
+      title: 'PDF file',
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
